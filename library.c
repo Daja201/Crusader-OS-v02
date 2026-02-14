6d780c713caa0598366574c539f499c526bc7022
David Zapletal <gejmr.daja@gmail.com>
2026-02-14 16:23:24 +0100

    Version 12 of COS - updated small things

--- library.c
+++ library.c
@@ -8,17 +8,23 @@ void library(void){
     klog("test library ran from library.c");
     klog("This is an simple operating system made by David Zapletal. Github: Daja201");
     klog("Here is simple library for using this operating system.");
+    //
+    klog("  BASIC:");
     klog("help - Use help for some basic info");
+    klog("clear - use clear to clear display");
     klog("cow - Use cow to make an cow jump through your window");
     klog("cat - Use to print cute cat");
-    klog("ld - use ld to detect and check drives");
-    klog("clear - use clear to clear display");
     klog("reboot - use reboot to reboot using triple fault");
-    klog("rt - use runtest to run runtest.c and make some runtests");
     klog("lib - bro u are using lib rn you should know what it does");
+    //
+    klog("  FS:");
+    klog("ld - use ld to detect and check drives");
     klog("read - use read to read from some files");
     klog("ls - use ls to read all files from directory");
-    klog("");
-    klog("");
+    klog("dl - dl deletes file, use dl <file>");
+    klog("wr - writes into file, use wr <file> <content>");
+    //
+    klog("  ADVANCED:");
+    klog("rt - use runtest to run runtest.c and make some runtests");
     //
 }
